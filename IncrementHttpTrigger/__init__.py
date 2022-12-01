f96c6adec4f8380f39d90d51dd7099cac44f03a2
WitheredGryphon <WitheredGryphon@gmail.com>
2022-11-30 23:20:29 -0600

    Add increment HTTP trigger function

---
+++ IncrementHttpTrigger/__init__.py
@@ -0,0 +1,31 @@
+import logging
+import azure.functions as func
+import json
+import uuid
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
+def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    URL = "https://counterhttpfunc.azurewebsites.net/api/counter/get"
+
+    try:
+        res = urlopen(URL)
+    except HTTPError as e:
+        return func.HttpResponse(f"An unexpected error accessing the API occurred. Error: {e}")
+
+    data = json.loads(res.read())
+    logging.info(data)
+
+    new_doc = func.DocumentList()
+
+    new_data = {
+        "id": "VisitCount",
+        "count": data['count'] + 1
+    }
+
+    new_doc.append(func.Document.from_dict(new_data))
+    doc.set(new_doc)
+
+    return func.HttpResponse("HTTP triggered function executed successfully.", status_code=200)
