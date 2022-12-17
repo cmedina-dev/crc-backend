99f28f725ce4ea81997939cdd8abbe9a4edfdf9d
WitheredGryphon <WitheredGryphon@gmail.com>
2022-12-16 23:53:07 -0600

    Undo json.loads change

--- IncrementHttpTrigger/__init__.py
+++ IncrementHttpTrigger/__init__.py
@@ -17,7 +17,7 @@ def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpRespon
 
     logging.info('Requested URL opened.')
 
-    data = res.read()
+    data = json.loads(res.read())
     logging.info(data)
 
     new_doc = func.DocumentList()
