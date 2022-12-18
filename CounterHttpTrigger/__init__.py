01fd4acaaa49ae3d8ce257004c058981f9d6d465
WitheredGryphon <WitheredGryphon@gmail.com>
2022-12-18 02:29:28 -0600

    Convert CounterHttpTrigger to async

--- CounterHttpTrigger/__init__.py
+++ CounterHttpTrigger/__init__.py
@@ -3,7 +3,7 @@ import json
 import azure.functions as func
 import os
 
-def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
+async def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
     #logging.info(doc[0].to_json())
     
