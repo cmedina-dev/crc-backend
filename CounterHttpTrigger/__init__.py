7bc85649731474f689e44d8efcc16f5fc79a4148
WitheredGryphon <WitheredGryphon@gmail.com>
2023-01-20 19:42:18 -0600

    Switched to Table entities for DB

--- CounterHttpTrigger/__init__.py
+++ CounterHttpTrigger/__init__.py
@@ -1,21 +1,37 @@
-import logging
-import json
-import azure.functions as func
 import os
+import copy
+import logging 
 
-async def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
-    logging.info("Python HTTP trigger function processed a request.")
-    #logging.info(doc[0].to_json())
+import azure.functions as func 
+
+def __init__(self):
+    self.connection_string = os.getenv("AzureCosmosDBConnectionString")
+    self.table_name = "crc-db"
+
+def fetchEntityData(self):
+    from azure.data.tables import TableClient
+    from azure.data.tables import UpdateMode
+    from azure.core.exceptions import ResourceExistsError
     
-    try:
-        count = doc[0]["count"]        
-    except IndexError as e:
-        count = 0
+    with TableClient.from_connection_string(self.connection_string, self.table_name) as table_client:
+        entity = {
+            "PartitionKey": "visitorpart",
+            "RowKey": "visitors",
+            "count": 400,
+        }
+        
+        try:
+            table_client.create_entity(entity = entity)
+            table_entity = table_client.get_entity(partition_key=entity["PartitionKey"], row_key=entity["RowKey"])
+        except ResourceExistsError:
+            table_entity = table_client.get_entity(partition_key=entity["PartitionKey"], row_key=entity["RowKey"])
+            table_entity["count"] += 1
+            
+        return table_entity
+        
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    table_entity = fetchEntityData()
+    return func.HttpResponse(f"{table_entity['count']}")
     
-    count_dict = {'count': count}
-    
-    return func.HttpResponse(
-        json.dumps(count_dict),
-        mimetype="application/json",
-        status_code=200
-    )
+if __name__ == "__main__":
+    main()
