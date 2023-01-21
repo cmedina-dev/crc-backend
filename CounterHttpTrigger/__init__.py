3f5160c8321814cd6b5b50585a7a72924c5e4459
WitheredGryphon <WitheredGryphon@gmail.com>
2023-01-20 20:05:56 -0600

    Add class encompassing functions

--- CounterHttpTrigger/__init__.py
+++ CounterHttpTrigger/__init__.py
@@ -4,34 +4,34 @@ import logging
 
 import azure.functions as func 
 
-def __init__(self):
-    self.connection_string = os.getenv("AzureCosmosDBConnectionString")
-    self.table_name = "crc-db"
+class CounterUpdater(object):
+    def __init__(self):
+        self.connection_string = os.getenv("AzureCosmosDBConnectionString")
+        self.table_name = "crc-db"
 
-def fetchEntityData(self):
-    from azure.data.tables import TableClient
-    from azure.data.tables import UpdateMode
-    from azure.core.exceptions import ResourceExistsError
-    
-    with TableClient.from_connection_string(self.connection_string, self.table_name) as table_client:
-        entity = {
-            "PartitionKey": "visitorpart",
-            "RowKey": "visitors",
-            "count": 400,
-        }
+    def fetchEntityData(self):
+        from azure.data.tables import TableClient
+        from azure.data.tables import UpdateMode
+        from azure.core.exceptions import ResourceExistsError
         
-        try:
-            table_client.create_entity(entity = entity)
-            table_entity = table_client.get_entity(partition_key=entity["PartitionKey"], row_key=entity["RowKey"])
-        except ResourceExistsError:
-            table_entity = table_client.get_entity(partition_key=entity["PartitionKey"], row_key=entity["RowKey"])
-            table_entity["count"] += 1
+        with TableClient.from_connection_string(self.connection_string, self.table_name) as table_client:
+            entity = {
+                "PartitionKey": "visitorpart",
+                "RowKey": "visitors",
+                "count": 400,
+            }
             
-        return table_entity
-        
+            try:
+                table_client.create_entity(entity = entity)
+                table_entity = table_client.get_entity(partition_key=entity["PartitionKey"], row_key=entity["RowKey"])
+            except ResourceExistsError:
+                table_entity = table_client.get_entity(partition_key=entity["PartitionKey"], row_key=entity["RowKey"])
+                table_entity["count"] += 1
+                
+            return table_entity
+            
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    table_entity = fetchEntityData()
+    counter = CounterUpdater()
+    table_entity = counter.fetchEntityData()
     return func.HttpResponse(f"{table_entity['count']}")
-    
-if __name__ == "__main__":
-    main()
