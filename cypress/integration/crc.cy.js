7fadec758cdca95a002a7c23098ac5855f69ac70
Christian Medina <117231596+cjmedina95@users.noreply.github.com>
2023-02-01 09:36:24 -0600

    Remove old increment function from Cypress test

--- cypress/integration/crc.cy.js
+++ cypress/integration/crc.cy.js
@@ -1,18 +1,7 @@
-describe('Get counter API', () => {
+describe('Check Counter API', () => {
 	it('Returns response code 200', () => {
 		cy.request({
 		  url: '/counter/get',
-		  followRedirect: false,
-		}).then((resp) => {
-		  expect(resp.status).to.eq(200)
-		})
-	})
-})
-
-describe('Increment counter API', () => {
-	it('Returns response code 200', () => {
-		cy.request({
-		  url: '/counter/increment',
 		  followRedirect: false,
 		}).then((resp) => {
 		  expect(resp.status).to.eq(200)
